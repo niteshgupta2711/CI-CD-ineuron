a5f7e20a599e40239197754bcdd5cf35f6cc364a
Nitesh kumar Gupta <guptanitesh2711@gmail.com>
2022-07-19 16:46:05 +0530

    flask initial app

---
+++ app.py
@@ -0,0 +1,11 @@
+
+from flask import Flask,render_template,request
+
+app=Flask(__name__)
+@app.route('/',methods=["GET"])
+def main():
+    return 'started machine learning project'
+    
+
+if __name__=='__main__':
+    app.run(debug=True)
